d958cdd8fd14d49c16ac7933a74499b44c7ad451
Felipe Marelli <felipemarelli@remind101.com>
2025-03-24 19:59:26 -0300

    Can load a movies file with one movie

--- backend/tests/test_imdb_visualizer.py
+++ backend/tests/test_imdb_visualizer.py
@@ -3,10 +3,17 @@ import unittest
 
 class ImdbVisualizer:
     def __init__(self, movies_csv_file):
-        pass
+        self._movies = []
+        with open(movies_csv_file, "r") as csv_file:
+            is_header = True
+            for line in csv_file:
+                if not is_header:
+                    self._movies.append(line)
+                else:
+                    is_header = False
 
     def amount_of_movies_loaded(self):
-        return 0
+        return len(self._movies)
 
 
 class ImdbVisualizerTestCase(unittest.TestCase):
@@ -18,6 +25,20 @@ class ImdbVisualizerTestCase(unittest.TestCase):
         imdb_visualizer = ImdbVisualizer(empty_movies_file)
 
         self.assertEqual(0, imdb_visualizer.amount_of_movies_loaded())
+
+    def test_can_load_one_movie_from_movies_file(self):
+        one_movie_file = "one_movie.csv"
+        with open(one_movie_file, "w") as csv_file:
+            csv_file.write(self._movies_file_csv_headers())
+            csv_file.write(self._titanic_movie_csv_entry())
+
+        imdb_visualizer = ImdbVisualizer(one_movie_file)
+
+        self.assertEqual(1, imdb_visualizer.amount_of_movies_loaded())
+
+    def _titanic_movie_csv_entry(self):
+        return "tt0000001,titanic,titanic original,1997,1997-01-01,action,120,usa,english,james cameron,\
+            james cameron,20th century fox,leonardo dicaprio,description,7.9,1,2,3,4,5,6,7\n"
 
     def _movies_file_csv_headers(self):
         return "imdb_title_id,title,original_title,year,date_published,genre,duration,country,language,director,\
