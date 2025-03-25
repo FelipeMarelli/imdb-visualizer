35e7059308596d9fd68fcc50197383bfc9ab86ef
Felipe Marelli <felipemarelli@remind101.com>
2025-03-25 10:37:50 -0300

    Refactor

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,7 @@
 import {useEffect, useState} from "react";
 import axios from "axios";
 import "./App.css";
-
-const API_BASE_URL = "http://127.0.0.1:8000";
+import {API_BASE_URL} from "./index";
 
 function App() {
     const [allMovies, setAllMovies] = useState([]);
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 
+export const API_BASE_URL = "http://127.0.0.1:8000";
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
